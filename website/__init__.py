00bb83aa1a1c906486eaffc2326ba1fa62e4595d
WahajK <wahajkhan788@gmail.com>
2024-12-29 12:55:28 +0100

    Added requirements.txt and redacted keys

--- website/__init__.py
+++ website/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from environment_variables import *
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = SECRET_KEY
-    app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_PUBLIC_KEY
-    app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_PRIVATE_KEY
+    app.config['SECRET_KEY'] = "XXXXXXXXXX"
+    app.config['RECAPTCHA_PUBLIC_KEY'] = "XXXXXXXXXXX"
+    app.config['RECAPTCHA_PRIVATE_KEY'] = "XXXXXXXXXXX"
     from .views import views
 
     app.register_blueprint(views,url_prefix = '/')
